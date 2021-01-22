2facd326afa7f6a2c9fbe19f1dc4207d68733ce7
DOT <DOT@ALI>
2021-01-22 10:28:21 +0300

    first commit.Gitignore ve .gitattributes ekle.

---
+++ limonmali/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using limonmali.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace limonmali.Controllers
+{
+    public class HomeController : Controller
+    {
+        dbLimonEntities db = new dbLimonEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hizmetler()
+        {
+            List<Hizmetler> hizmetler = db.Hizmetler.ToList();
+
+            return PartialView(hizmetler);
+        }
+        public ActionResult Hakkimizda()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+        public ActionResult Slider()
+        {
+            List<Slider> slider = db.Slider.ToList();
+
+            return PartialView(slider);
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
